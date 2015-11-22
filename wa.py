a68f8990396446339a2352ecf8437a9f9d4e1b2f
sam w <sam@foofrancine.com>
2015-11-22 01:28:53 +0000

    update db test

--- wa.py
+++ wa.py
@@ -1,8 +1,32 @@
 import psycopg2
  
 import sys
+import time
+from datetime import datetime
+import random
+import re
+import match
+import os
+import sendemail
+import pclogging
+
+sys.path.append('./Adafruit_Python_GPIO')
+sys.path.append('./RTC_SDL_DS3231')
+sys.path.append('./Adafruit_Python_BMP')
+sys.path.append('./SDL_Pi_Weather_80422')
+sys.path.append('./RaspberryPi-AS3935/RPi_AS3935')
+sys.path.append('./SDL_Pi_INA3221')
+sys.path.append('./SDL_Pi_TCA9545')
+
+import SDL_Pi_INA3221
+import SDL_DS3231
+import Adafruit_BMP.BMP085 as BMP180
+import SDL_Pi_Weather_80422 as SDL_Pi_Weather_80422
+
  
 db_con = None
+
+dt = datetime.now()
  
 try:
  
@@ -18,7 +42,7 @@ try:
  
   #The variables placeholder must always be a %s, psycop2 will automatically convert the values to SQL literal
  
-  cursor.execute("INSERT INTO weathers ( temperature, humidity, content) VALUES (%s, %s, %s)",(12, 26, "psycho"))
+  cursor.execute("INSERT INTO weathers ( temperature, humidity, content, created_at) VALUES (%s, %s, %s, %s)",(12, 26, "psycho", dt ))
  
   db_con.commit()
  
